0bf954edf4b16ecdee9900944c0316463f44926d
Hp <Hp@DESKTOP-QATO3AJ>
2024-07-19 17:30:03 +0530

    Commit

---
+++ CONFESSION1/src/main/java/com/cms/confession/service/ConfessionAppointmentService.java
@@ -0,0 +1,47 @@
+package com.cms.confession.service;
+
+import com.cms.confession.entity.ConfessionAppointment;
+import com.cms.confession.repository.ConfessionAppointmentRepository;
+
+
+import jakarta.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ConfessionAppointmentService {
+
+    @Autowired
+    private ConfessionAppointmentRepository repository;
+    
+
+    public List<ConfessionAppointment> getAllAppointments() {
+        return repository.findAll();
+    }
+
+    public Optional<ConfessionAppointment> getAppointmentById(Long id) {
+        return repository.findById(id);
+    }
+
+    public ConfessionAppointment saveAppointment(ConfessionAppointment appointment) {
+        return repository.save(appointment);
+    }
+
+    public void deleteAppointment(Long id) {
+        repository.deleteById(id);
+    }
+
+	
+	
+	
+	public ConfessionAppointment updateConfessionAppointment(Integer id,ConfessionAppointment request) {
+		return null;
+	}
+
+	
+	
+}
